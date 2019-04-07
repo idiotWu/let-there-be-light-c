8ccbbccb512afcfc37a49cb4655ad6d5efb7fa5e
Dolphin Wood <dolphin.w.e@gmail.com>
2019-04-07 20:29:17 +0900

    Initial Commit

---
+++ let-there-be-light-c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  let-there-be-light-c
+//
+//  Created by Dolphin Wood on 2019/04/07.
+//  Copyright © 2019 Dolphin Wood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
